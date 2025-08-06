5ce558320fc3243ad01c4babf137fad2a5d95b13
hwanheejung <hwanheejung117@gmail.com>
2025-08-06 18:55:49 +0900

    🔧 빌드된 파일에 css을 주입하여 빌드한다

--- vite.config.ts
+++ vite.config.ts
@@ -3,55 +3,51 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import libCss from 'vite-plugin-libcss';
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const isDev = command === 'serve';
-  
+export default defineConfig(() => {
   return {
     plugins: [
       react(), 
-      vanillaExtractPlugin(),
-      ...(isDev ? [] : [
-        dts({
-          tsconfigPath: './tsconfig.build.json',
-          insertTypesEntry: true,
-          rollupTypes: true,
-          outDir: 'dist',
-        }),
-      ]),
+      vanillaExtractPlugin({ identifiers: 'short' }),
+      dts({
+        tsconfigPath: './tsconfig.build.json',
+        insertTypesEntry: true,
+        rollupTypes: true,
+        outDir: 'dist',
+      }),
+      libCss(),
     ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-    ...(isDev ? {} : {
-      build: {
-        lib: {
-          entry: path.resolve(__dirname, 'src/index.ts'),
-          name: 'ApiRecorderDevtools',
-          formats: ['es'],
-          fileName: (format) => `api-recorder.${format}.js`,
-        },
-        rollupOptions: {
-          external: [
-            'react',
-            'react-dom',
-            'react/jsx-runtime',   
-            'socket.io-client',
-          ],
-          output: {
-            globals: {
-              react: 'React',
-              'react-dom': 'ReactDOM',
-              'socket.io-client': 'SocketIO',
-            },
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'src/index.ts'),
+        name: 'ApiRecorderDevtools',
+        formats: ['es'],
+        fileName: (format) => `api-recorder.${format}.js`,
+      },
+      cssCodeSplit: false,
+      rollupOptions: {
+        external: [
+          'react',
+          'react-dom',
+          'react/jsx-runtime',   
+          'socket.io-client',
+        ],
+        output: {
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+            'socket.io-client': 'SocketIO',
           },
         },
-        cssCodeSplit: false,
-        cssMinify: true,
       },
-    }),
+      
+    },
   };
 });
